3ad2e0199da31d4561c5a908c28d8f1d668afee7
Denis Sklyarov <sklyarov.fortech@gmail.com>
2022-09-23 15:50:29 +0300

    task8fixes

--- EightTask.js
+++ EightTask.js
@@ -1,13 +1,20 @@
-const arr8 = [
-    { name: 'Ivan', age: 24 },
-    { name: 'Oleg', age: 16}, 
-    { name: 'Igor', age: 24}
+const array8 = [
+  { name: 'Ivan', age: 24 },
+  { name: 'Oleg', age: 16 }, 
+  { name: 'Igor', age: 24 }
 ];
-
-let sum = 0;
-
-for(let i = 0; i < arr8.length; i++){
-    sum += arr8[i].age;
-}
-
-console.log(sum)
+  
+const FunctionTotalNumberOfYears = (array) => {
+  
+  let sum = 0;
+  
+  for(let i = 0; i < array.length; i++){
+    sum += array[i].age;
+  }
+  return sum
+};
+  
+console.log(FunctionTotalNumberOfYears(array8))
+  
+// Имеется массив пользователей вида [{ name: “Ivan”, age: 24 }]. 
+// Вывести суммарный возраст всех пользователей.
