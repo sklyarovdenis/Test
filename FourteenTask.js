cba169eb54795d3332877aacc32bd971aae135ce
Denis Sklyarov <sklyarov.fortech@gmail.com>
2022-09-23 14:15:43 +0300

    task14fixes

--- FourteenTask.js
+++ FourteenTask.js
@@ -1,17 +1,17 @@
 const functionArithmeticOperations = (num1, num2, operation) => {
   switch (operation) {
-      case '+':
-          return num1 + num2;
-          break;
-      case '-':
-          return num1 - num2;
-          break;
-      case '*':
-          return num1 * num2;
-          break;
-      case '/':
-          return num1 / num2;
-          break;
+    case '+':
+      return num1 + num2;
+      break;
+    case '-':
+      return num1 - num2;
+      break;
+    case '*':
+      return num1 * num2;
+      break;
+    case '/':
+      return num1 / num2;
+      break;
   }
 };
 
