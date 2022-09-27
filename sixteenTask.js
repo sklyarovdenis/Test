17df4447f0cef27b82879140265f05a7ac4b01fb
Denis Sklyarov <sklyarov.fortech@gmail.com>
2022-09-27 18:14:56 +0300

    task16

---
+++ sixteenTask.js
@@ -0,0 +1,9 @@
+const numberTelephone =(number) => {
+   console.log(`${number[0]}(${number[1]}${number[2]}${number[3]})${number[4]}${number[5]}${number[6]}-${number[7]}${number[8]}-${number[9]}${number[10]}`)
+}
+
+numberTelephone('88005553535')
+
+
+// Написать функцию, принимающую на вход неотформатированный номер телефона 
+// и возвращающую его в отформатированном виде.
